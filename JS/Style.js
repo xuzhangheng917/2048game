db4b436c38b7b1d41070294f4c0e97204d96921c
22362 <2236232192@qq.com>
2024-06-21 08:51:26 +0800

    1.5

--- JS/Style.js
+++ JS/Style.js
@@ -1,14 +1,19 @@
 const buttons =
     document.querySelectorAll(".GameDifficulty button")
 
+const oldButton = document.querySelector('.GameDifficulty .OldMode button')
+
 let LastButton = buttons[0]
 
 buttons.forEach(
     (item) =>{
-        item.addEventListener("click",(event) => {
+        item.addEventListener('click',(event) => {
+            if (item.innerText === '老年模式') return
             LastButton.classList.remove('ButtonClick')
             event.target.classList.add('ButtonClick')
             LastButton = event.target
         })
     }
 )
+
+oldButton.addEventListener('click',(ev) => ev.target.classList.toggle('ButtonClick'))
